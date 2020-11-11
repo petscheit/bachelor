d7e78ce1f72829364795319f1a2871fb7f38b734
paul etscheit <etscheitp@googlemail.com>
2020-11-11 13:55:24 +0100

    adds contract integration

--- implementation/client/src/App.js
+++ implementation/client/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import getWeb3 from "./helpers/getWeb3.js";
+import ZkSwap from "./contracts/ZkSwap.json";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount = async () => {
+        try {
+            // Get network provider and web3 instance.
+            let web3 = await getWeb3();
+            const accounts = await web3.eth.getAccounts();
+            const networkId = await web3.eth.net.getId();
+            const deployedNetwork = ZkSwap.networks[networkId];
+            const instance = new web3.eth.Contract(
+              ZkSwap.abi,
+              deployedNetwork.address,
+            );
+            console.log(instance)
+           
+            // invokeListener(instance, accounts[0], web3);
+        } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+            `Failed to load web3, accounts, or contract. Check console for details.`,
+        );
+        console.error(error);
+        }
+    };
+
+
+
+    render() {
+      return("yeahhh")
+    }
 }
+
+
+// export default App;
+const mapStateToProps = state => {
+    // return {
+    //     account: state.user.account,
+    //     web3: state.web3.instance,
+    //     projectContract: state.web3.projectContract,
+    //     address: state.project.address,
+    // }
+};
+
+const mapDispatchToProps = dispatch => ({
+  // addAccount: account => dispatch(addAccount(account)),
+  // addWeb3: instance => dispatch(addWeb3(instance)),
+  // addProjectCont: instance => dispatch(addProjectCont(instance)),
+  // addProjectAddress: address => dispatch(addProjectAddress(address)),
+//   addGroupedPaymentUnits: 
+})
+
+
+// export default connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+// )(App);
 
 export default App;
--- implementation/contracts/truffle-config.js
+++ implementation/contracts/truffle-config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   // Uncommenting the defaults below 
   // provides for an easier quick-start with Ganache.
@@ -5,6 +7,7 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
+    contracts_build_directory: path.join(__dirname, "../client/src/contracts"),
     networks: {
         development: {
         host: "localhost",     // Localhost (default: none)
