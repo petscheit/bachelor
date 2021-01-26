db3f54bd825ab8df051380e9aea18573c1636354
paul etscheit <etscheitp@googlemail.com>
2021-01-26 11:53:11 +0100

    adds verifier contract

--- implementation/contracts/migrations/2_deploy_contracts.js
+++ implementation/contracts/migrations/2_deploy_contracts.js
@@ -1,9 +1,11 @@
 const ZkSwap = artifacts.require("ZkSwap");
 const Token = artifacts.require("Token");
+const Verifier = artifacts.require("Verifier")
 
 module.exports = async function(deployer) {
-    await deployer.deploy(Token).then(inst => {
-      let addrs = inst.address
-      return deployer.deploy(ZkSwap, addrs, addrs);
+    await deployer.deploy(Token).then(async inst => {
+      let tokenAddrs = inst.address
+      let verifierAddrs = await deployer.deploy(Verifier).then(instance => instance.address)
+      return deployer.deploy(ZkSwap, tokenAddrs, verifierAddrs);
     })
 };
